af10dc07137d34451a58b51b90730a6af5a282ae
grace-kimotho <graycenjoki@gmail.com>
2017-09-25 12:38:37 +0300

    add environment configurations

---
+++ instance/config.py
@@ -0,0 +1,44 @@
+import os
+
+class Config(object):
+	"""
+	Base configuration class
+	"""
+	DEBUG = False
+	CSRF_ENABLED = True
+	SECRET = os.getenv('SECRET')
+	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+	"""
+	Development configurations
+	"""
+	DEBUG = True
+
+class TestingConfig(Config):
+	"""
+	Testing configurations
+	"""
+	TESTING = True
+	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2.9.0.1.@localhost/db_name'
+	DEBUG = True
+
+class StagingConfig(Config):
+	"""
+	Staging configurations
+	"""
+	DEBUG = True
+
+class ProductionConfig(Config):
+	"""
+	Production configurations
+	"""
+	DEBUG = False
+	TESTING = False
+
+app_config = {
+	'development': DevelopmentConfig,
+	'testing': TestingConfig,
+	'staging': StagingConfig,
+	'production': ProductionConfig,
+}
