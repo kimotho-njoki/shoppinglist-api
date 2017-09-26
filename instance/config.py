2b613a299a5855ff4068de617ca64ee0d06ef972
grace-kimotho <graycenjoki@gmail.com>
2017-09-26 20:58:49 +0300

    modify configurations

--- instance/config.py
+++ instance/config.py
@@ -1,44 +1,44 @@
 import os
 
 class Config(object):
-	"""
-	Base configuration class
-	"""
-	DEBUG = False
-	CSRF_ENABLED = True
-	SECRET = os.getenv('SECRET')
-	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2.9.0.1.@localhost/shoplisttest_db'
+    """
+    Base configuration class
+    """
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2.9.0.1.@localhost/shoplisttest_db'
 
 class DevelopmentConfig(Config):
-	"""
-	Development configurations
-	"""
-	DEBUG = True
+    """
+    Development configurations
+    """
+    DEBUG = True
 
 class TestingConfig(Config):
-	"""
-	Testing configurations
-	"""
-	TESTING = True
-	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2.9.0.1.@localhost/shoplisttest_db'
-	DEBUG = True
+    """
+    Testing configurations
+    """
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2.9.0.1.@localhost/shoplisttest_db'
+    DEBUG = True
 
 class StagingConfig(Config):
-	"""
-	Staging configurations
-	"""
-	DEBUG = True
+    """
+    Staging configurations
+    """
+    DEBUG = True
 
 class ProductionConfig(Config):
-	"""
-	Production configurations
-	"""
-	DEBUG = False
-	TESTING = False
+    """
+    Production configurations
+    """
+    DEBUG = False
+    TESTING = False
 
 app_config = {
-	'development': DevelopmentConfig,
-	'testing': TestingConfig,
-	'staging': StagingConfig,
-	'production': ProductionConfig,
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
 }
