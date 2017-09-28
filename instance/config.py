d8735d59fabd6ea2d2581eb1ff17b68b9b35ee96
grace-kimotho <graycenjoki@gmail.com>
2017-09-28 16:00:59 +0300

    [Fixes] Fix errors in code

--- instance/config.py
+++ instance/config.py
@@ -7,7 +7,7 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2.9.0.1.@localhost/shoplisttest_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2.9.0.1.@localhost/shoplistapi_db'
 
 class DevelopmentConfig(Config):
     """
@@ -20,7 +20,7 @@ class TestingConfig(Config):
     Testing configurations
     """
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2.9.0.1.@localhost/shoplisttest_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2.9.0.1.@localhost/shoplistapi_db'
     DEBUG = True
 
 class StagingConfig(Config):
