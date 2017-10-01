723449ed2eec2d5c20830609e19792af73791715
grace-kimotho <graycenjoki@gmail.com>
2017-10-01 20:42:01 +0300

    [Refactor] automate tests

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
@@ -9,6 +10,17 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.command 
+def test():
+	"""
+	Run unit tests
+	"""
+	tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+	result = unittest.TextTestRunner(verbosity=2).run(tests)
+	if result.wasSuccessful():
+		return 0
+	return 1
 
 if __name__ == '__main__':
 	manager.run()
