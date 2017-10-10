d6c04e45bfefa7d0667b94acf53d01bf1d7594d8
Grace Njoki <gracenjoki@Graces-MacBook-Pro.local>
2017-10-10 17:03:36 +0300

    [Chore] make messages more descriptive

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,7 +19,7 @@ class RegistrationView(MethodView):
         if re.match("^[a-zA-Z0-9 _]*$", username) and username.strip():
             if re.match(regex, email) and email.strip():
                 if len(password) > 6 and password.strip():
-                    user = User.query.filter_by(username=username).first()
+                    user = User.query.filter_by(username=username, email=email).first()
 
                     if not user:
                         try:
@@ -44,20 +44,20 @@ class RegistrationView(MethodView):
                         return make_response(jsonify(response)), 202
                 else:
                     response = {
-                    'message': "Password is either too short or empty.\
-            Please try again."
+                    'message': "Password is either too short or empty."\
+            "Please try again."
                     }
                     return make_response(jsonify(response)), 403
             else:
                 response = {
-                'message': "Email Invalid.\
-            Do not include special characters or leave the field empty."
+                'message': "Email Invalid"\
+            "Do not include special characters or leave the field empty."
                 }
                 return make_response(jsonify(response)), 403
         else:
             response = {
-            'message': "Username can neither include special\
-            characters nor be empty."
+            'message': "Username can neither include special"\
+            "characters nor be empty."
             }
             return make_response(jsonify(response)), 403
 
@@ -76,7 +76,7 @@ class LoginView(MethodView):
             if email.strip():
                 if password.strip():
                     try:
-                        user = User.query.filter_by(username=username).first()
+                        user = User.query.filter_by(username=username, email=email).first()
                         if user and user.password_is_valid(password):
                             access_token = user.encode_auth_token(user.id)
                             if access_token:
@@ -88,8 +88,8 @@ class LoginView(MethodView):
                                 return make_response(jsonify(response)), 200
                         else:
                             response = {
-                            'message': "Login unsuccessful.\
-            Please register or confirm details."
+                            'message': "Login unsuccessful."\
+            "Please register or confirm details."
                             }
                             return make_response(jsonify(response)), 401
                     except Exception as e:
